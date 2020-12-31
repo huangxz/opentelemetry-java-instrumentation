053601538bbf3b4c44be7b0c89d72bc7d17910e2
rahul.r <rahul.r@PENNANT-VSP103.pennant.local>
2020-12-31 12:20:38 +0530

    Exporters

---
+++ javaagent-exporters/jaeger/src/main/java/io/opentelemetry/javaagent/exporters/jaeger/JaegerExporterFactory.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.exporters.jaeger;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.exporter.jaeger.JaegerGrpcSpanExporter;
+import io.opentelemetry.exporter.jaeger.proto.api_v2.Model;
+import io.opentelemetry.javaagent.spi.exporter.SpanExporterFactory;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.Set;
+
+@AutoService(SpanExporterFactory.class)
+public class JaegerExporterFactory implements SpanExporterFactory {
+	
+private  Model.Process.Builder processBuilder;
+	
+  @Override
+  public SpanExporter fromConfig(Properties config) {
+    return JaegerGrpcSpanExporter.builder().readProperties(config).build();
+  }
+
+  @Override
+  public Set<String> getNames() {
+    return Collections.singleton("jaeger");
+  }
+  
+}
